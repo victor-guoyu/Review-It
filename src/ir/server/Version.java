f8812057833e0b6af7e2f01b132af09897561431
Victor Guo <victor.guoyu@hotmail.com>
2015-02-20 16:17:25 -0500

    Added Version

---
+++ src/ir/server/Version.java
@@ -0,0 +1,10 @@
+package ir.server;
+
+import java.util.Date;
+
+public class Version {
+    public static final String AppServerVersion = "1.0.0";
+    public static final String AppServerBuild = "1";
+    public static final String AppServerMessage = "CSI4900 Honours Project";
+    public static Date upSince = null;
+}
