d943b603b259a24f0706383d20086fe169006145
Zhiting Lin <zlin035@uottawa.ca>
2015-03-29 23:46:03 -0400

    AWSCrawler rework

--- src/ir/crawler/amazon/AWSCrawler.java
+++ src/ir/crawler/amazon/AWSCrawler.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map.Entry;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -19,7 +18,10 @@ import com.google.common.primitives.Ints;
 
 import ir.config.Configuration;
 import ir.crawler.Crawler;
+import ir.crawler.Source;
+import ir.crawler.UUIDgenerator;
 import ir.index.ParsedComment;
+import ir.index.SearchEngine;
 import static ir.crawler.amazon.AWSRequestFileds.*;
 
 /**
@@ -31,7 +33,7 @@ import static ir.crawler.amazon.AWSRequestFileds.*;
  */
 
 public class AWSCrawler extends Crawler {
-    private static final int     MAX_PAGE_NUM = 10;
+    private static final int     MAX_PAGE_NUM = 1;
     private Configuration        config;
     private Logger               logger;
     private SignedRequestsHelper helper;
@@ -43,82 +45,106 @@ public class AWSCrawler extends Crawler {
             init();
         }
         queries.stream().forEach((query) -> {
-               retrieveContent(query);
+            retrieveContent(query);
         });
     }
 
-    public void init()  {
+    public void init() {
         config = Configuration.getInstance();
         logger = LogManager.getLogger(AWSCrawler.class);
         try {
-            helper = SignedRequestsHelper
-                    .getInstance(
-                            config.getAwsEndPoint(),
-                            config.getAwsAccessKeyId(),
-                            config.getAwsSecretKey());
+            helper = SignedRequestsHelper.getInstance(config.getAwsEndPoint(),
+                    config.getAwsAccessKeyId(), config.getAwsSecretKey());
         } catch (Exception e) {
             throw new RuntimeException("AWSCrawler: unable to create helper", e);
         }
     }
 
+    /**
+     * Retrieve comments based on user query
+     * 
+     * @param query
+     */
     private void retrieveContent(String query) {
-        pageLinksToVisit = createURLs();
+        pageLinksToVisit = createURLs(query);
         List<ParsedComment> comments = Lists.newLinkedList();
         for (String link : pageLinksToVisit) {
-           List<ParsedComment>  pageComments = retrievePageComments(link);
+            List<ParsedComment> pageComments = retrievePageComments(link);
+            comments.addAll(pageComments);
+        }
+        if (!comments.isEmpty()) {
+            SearchEngine.getSearchEngine().indexDocuments(comments);
         }
     }
 
     /**
      * Create the URLs
      */
-    private String [] createURLs() {
-        String[] linksToVisit = new String [MAX_PAGE_NUM];
+    private String[] createURLs(String query) {
+        String[] linksToVisit = new String[MAX_PAGE_NUM];
 
         HashMap<String, String> params = Maps.newHashMap();
         params.put(PARAM_KEY_ASSOCIATETAG, "com0fd-20");
         params.put(PARAM_KEY_SERVICE, PARAM_VALUE_SERVICE);
         params.put(PARAM_KEY_OPERATION, PARAM_VALUE_OPERATION);
         params.put(PARAM_KEY_SEARCHINDEX, "All");
-        params.put(PARAM_KEY_KEYWORDS, "macbook");
+        params.put(PARAM_KEY_KEYWORDS, query);
         params.put(PARAM_KEY_INCLUDEREVIEWS, PARAM_VALUE_INCLUDEREVIEWS);
 
-        for(int i=0; i< MAX_PAGE_NUM; i++) {
-            params.put(PARAM_KEY_ITEMPAGE, String.valueOf(i+1));
+        for (int i = 0; i < MAX_PAGE_NUM; i++) {
+            params.put(PARAM_KEY_ITEMPAGE, String.valueOf(i + 1));
             linksToVisit[i] = helper.sign(params);
         }
         return linksToVisit;
     }
 
     private List<ParsedComment> retrievePageComments(String url) {
+        List<ParsedComment> pageComment = Lists.newLinkedList();
         try {
             Document page = Jsoup.connect(url).get();
-            List<ParsedComment> pageComment = Lists.newLinkedList();
             Elements items = page.getElementsByTag(ITEM_TAG);
             Iterator<Element> itemsIterator = items.iterator();
             Elements errors = page.getElementsByTag(ERROR_TAG);
             while (itemsIterator.hasNext() && errors.isEmpty()) {
                 Element item = itemsIterator.next();
-                String title = item.getElementsByTag(TITLE_TAG).get(FIRST_ELEMENT).text();
+                String title = item.getElementsByTag(TITLE_TAG)
+                        .get(FIRST_ELEMENT).text();
                 for (Element itemLink : item.getElementsByTag(ITEMLINK_TAG)) {
-                    if (itemLink.getElementsByTag(DESCRIPTION_TAG).get(FIRST_ELEMENT).text()
+                    if (itemLink.getElementsByTag(DESCRIPTION_TAG)
+                            .get(FIRST_ELEMENT).text()
                             .contains("Customer Reviews")) {
-                        titleToReviews.put(title, itemLink.getElementsByTag(URL_TAG)
-                                .get(FIRST_ELEMENT).text());
+
+                        pageComment.addAll(RetriveCustomerReview(
+                                title,
+                                itemLink.getElementsByTag(URL_TAG)
+                                        .get(FIRST_ELEMENT).text()));
                     }
                 }
             }
         } catch (IOException e) {
-
+            String msg = String.format("Unable to parse link %s", url);
+            logger.error(msg);
         }
+        return pageComment;
+
     }
 
-    private void retriveReviews(Document doc) {
-        Elements reviewList = doc.select("div.reviewText");
-        for (int i = 0; i < reviewList.size(); i++) {
-            String msg = reviewList.get(i).html();
-            logger.info(msg);
+    private List<String> retriveReviews(String pagingUrl) {
+        List<String> commentlist = Lists.newLinkedList();
+        try {
+            Document doc = Jsoup.connect(pagingUrl).get();
+            Elements reviewList = doc.select("span.review-text");
+            for (int i = 0; i < reviewList.size(); i++) {
+                String msg = reviewList.get(i).html();
+                commentlist.add(msg);
+            }
+
+        } catch (IOException e) {
+            String msg = String.format("Unable to parse link %s", pagingUrl);
+            logger.error(msg);
         }
+        return commentlist;
+
     }
 
     /**
@@ -127,30 +153,52 @@ public class AWSCrawler extends Crawler {
      *            : The URL that need to be extracted
      * @throws Exception
      */
-    private void RetriveCustomerReview(String ReviewURL) throws Exception {
-        Document doc = Jsoup.connect(ReviewURL).get();
+    private List<ParsedComment> RetriveCustomerReview(String productTitle,
+            String ReviewURL) {
+        Document doc;
+        List<ParsedComment> Comments = Lists.newLinkedList();
+        try {
+            doc = Jsoup.connect(ReviewURL).get();
+            // retrive the reference to the paging url
+            Elements reviewpageURLList = doc
+                    .select("#cm_cr-pagination_bar > ul > li> a");
+            // total number of page in the web page.
+            Integer num = 1;
+            String CommonPagingURL = "";
 
-        // retrive the reference to the page url
-        Elements reviewpageURLList = Jsoup.connect(ReviewURL).get()
-                .select(".paging a");
-        // total number of page in the web page.
-        Integer num = 1;
-        String pagingURL = "Not paging avalable";
-
-        for (int j = 0; j < reviewpageURLList.size(); j++) {
-            Integer temp = Ints.tryParse(reviewpageURLList.get(j).text());
-            if (temp != null && temp > num) {
-                num = temp;
-                pagingURL = reviewpageURLList.get(j).attr("href");
+            for (int j = 0; j < reviewpageURLList.size(); j++) {
+                Integer temp = Ints.tryParse(reviewpageURLList.get(j).text());
+                // find the paging with max number
+                if (temp != null && temp > num) {
+                    num = temp;
+                    CommonPagingURL = reviewpageURLList.get(j).attr("href");
+                }
             }
+
+            if (reviewpageURLList.isEmpty()) {
+                logger.info("Not paging avalable");
+            } else {
+
+                for (int i = 1; i <= num; i++) {
+                    String pagingURL = "http://www.amazon.com"
+                            + CommonPagingURL.replace("?ie=UTF8",
+                                    "/ref=cm_cr_pr_btm_link_" + i).replace(
+                                    "pageNumber=" + num, "pageNumber=" + i);
+                    List<String> commentlist = retriveReviews(pagingURL);
+                    for (String comment : commentlist) {
+                        Comments.add(new ParsedComment.Builder(UUIDgenerator
+                                .get(), Source.AMAZON)
+                                .productName(productTitle).comment(comment)
+                                .commentUrl(pagingURL).build());
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            String msg = String.format("Unable to parse link %s", ReviewURL);
+            logger.error(msg);
         }
-        // pagingURL = reviewpageURLList.first().attr("href");
-        logger.info(num);
-        logger.info(pagingURL);
 
-        // for loop to retrieve all the customer reviews.
-
-        // retrive review
-        retriveReviews(doc);
+        return Comments;
     }
 }
