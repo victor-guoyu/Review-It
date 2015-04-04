93c1bed5dbc6eaaf81f7535dc037d2cf5e86d2cd
Victor Guo <victor.guoyu@hotmail.com>
2015-04-04 16:07:59 -0400

    #16 Added YouTube video request handler

--- src/ir/handler/SearchVideoHandler.java
+++ src/ir/handler/SearchVideoHandler.java
@@ -6,46 +6,43 @@ import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
 import com.thetransactioncompany.jsonrpc2.JSONRPC2Response;
 import com.thetransactioncompany.jsonrpc2.server.MessageContext;
 import com.thetransactioncompany.jsonrpc2.server.RequestHandler;
-import ir.crawler.twitter.Tweet;
+import ir.crawler.youtube.Video;
 import ir.index.SearchEngine;
 
-import java.util.List;
 import java.util.Map;
 
-public class SearchTweetHandler implements RequestHandler {
-
-    protected static final String TWEET_SEARCH = "tweetSearch";
+public class SearchVideoHandler implements RequestHandler{
+    protected static final String VIDEO_SEARCH = "videoSearch";
     protected static final String SEARCH_TEXT = "text";
 
     @Override
     public String[] handledRequests() {
-        return new String[]{TWEET_SEARCH};
+        return new String[]{VIDEO_SEARCH};
     }
 
     @Override
     public JSONRPC2Response process(JSONRPC2Request request, MessageContext messageContext) {
-        if (request.getMethod().equalsIgnoreCase(TWEET_SEARCH)) {
-            return handleTweetSearch(request);
+        if (request.getMethod().equalsIgnoreCase(VIDEO_SEARCH)) {
+            return handleVideoSearch(request);
         } else {
             return new JSONRPC2Response(JSONRPC2Error.METHOD_NOT_FOUND, request.getID());
         }
     }
 
     /**
-     * Twitter Search handler
+     * YouTube search handler
      *
      * @param request
-     * @return List of Tweet in JSON
+     * @return Top rated YouTube video
      */
-    private JSONRPC2Response handleTweetSearch(JSONRPC2Request request) {
+    private JSONRPC2Response handleVideoSearch(JSONRPC2Request request) {
         Map<String, Object> param = request.getNamedParams();
         if (param != null) {
             String text = String.valueOf(param.get(SEARCH_TEXT));
             if (!Strings.isNullOrEmpty(text)) {
-                List<Tweet> tweets = SearchEngine.getSearchEngine().getTopTweets(text);
-                return new JSONRPC2Response(tweets, request.getID());
+                Video topVideo = SearchEngine.getSearchEngine().getTopRatedVideo(text);
+                return new JSONRPC2Response(topVideo, request.getID());
             }
-
         }
         return new JSONRPC2Response(JSONRPC2Error.INVALID_PARAMS, request.getID());
     }
