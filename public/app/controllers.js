2de0d76ff9a0979193637f7a93e6518b614d39ef
Victor Guo <victor.guoyu@hotmail.com>
2015-04-08 14:56:44 -0400

    #20 Fixed search bar

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,11 +1,14 @@
 angular.module('searchApp')
-    .controller('indexController',['$scope' ,'$state' ,function($scope, $state){
+    .controller('indexController',['$scope', '$state', function($scope, $state){
         $scope.search = function() {
             $state.go('result',{text: $scope.text});
         };
     }])
-    .controller('resultController',['$scope', '$stateParams', 'reviews',
-        function($scope, $stateParams, reviews){
+    .controller('resultController',['$scope', '$stateParams', 'reviews', '$state',
+        function($scope, $stateParams, reviews, $state){
             $scope.reviews = reviews;
             $scope.search = $stateParams.text;
+            $scope.newSearch = function() {
+                $state.go('result',{text: $scope.text});
+            };
         }]);
