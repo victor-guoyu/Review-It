98076bf18f8ae2557c9bcdc9f26402e36512b9c8
Victor Guo <victor.guoyu@hotmail.com>
2015-04-08 12:52:47 -0400

    JS structure refactor

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,7 +1,11 @@
 angular.module('searchApp')
+    .controller('indexController',['$scope' ,'$state' ,function($scope, $state){
+        $scope.search = function() {
+            $state.go('result',{text: $scope.text});
+        };
+    }])
     .controller('resultController',['$scope', '$stateParams', 'reviews',
         function($scope, $stateParams, reviews){
-            window.t = reviews;
             $scope.reviews = reviews;
             $scope.search = $stateParams.text;
-    }]);
+        }]);
--- public/app/controllers/index-controller.js
+++
@@ -1,6 +0,0 @@
-angular.module('searchApp')
-    .controller('indexController',['$scope' ,'$state' ,function($scope, $state){
-        $scope.search = function() {
-          $state.go('result',{text: $scope.text});
-        };
-    }]);
